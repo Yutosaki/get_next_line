6a0d1d8e62cb8bd390bfac1d4a0be75d72502ea4
Yuto Sasaki <yutsasak@c1r4s3.42tokyo.jp>
2024-09-21 12:26:34 +0900

    feat: add header

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,28 +6,28 @@
 /*   By: yutsasak <yutsasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 14:59:20 by yutsasak          #+#    #+#             */
-/*   Updated: 2024/07/03 21:20:20 by yutsasak         ###   ########.fr       */
+/*   Updated: 2024/09/21 12:24:04 by yutsasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 # endif
 
+# include <limits.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <limits.h>
 
 # if BUFFER_SIZE > 2147483646 || BUFFER_SIZE < 0
 #  undef BUFFER_SIZE
 #  define BUFFER_SIZE 0
+
 # endif
 
 char	*get_next_line(int fd);
-
 char	*ft_strjoin(const char *s1, const char *s2);
 char	*ft_strdup(const char *s1);
 char	*ft_strchr(const char *s, int c);
